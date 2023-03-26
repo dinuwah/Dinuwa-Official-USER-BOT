a99005c80e1e3db09ce2df09e82ff0b0bc223473
dinuboza <121083548+dinuboza@users.noreply.github.com>
2023-03-26 09:38:08 +0530

    Update youtube-ytvd-o.js

--- plugins/youtube-ytvd-o.js
+++ plugins/youtube-ytvd-o.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, groupMetadata, usedPrefix, text, args, command }
  `
 let buttons = [{ buttonText: { displayText: 'Video' }, buttonId: `${usedPrefix}ytv ${text} ` }, { buttonText: { displayText: 'Document' }, buttonId: `${usedPrefix}vdoc ${text} ` }]
 							 
-let msg = await conn.sendMessage(m.chat, { image: { url: 'https://i.ibb.co/cc8jfkX/01.jpg' }, caption: capt, footer: '_*Whatsapp bot by AHMAD ALI*_', buttons }, { quoted: m })
+let msg = await conn.sendMessage(m.chat, { image: { url: 'https://i.ibb.co/T1DqdYz/thumbnail.jpg' }, caption: capt, footer: '_*Whatsapp bot by Dinuwa Official²⁰²³*_', buttons }, { quoted: m })
 }
 
 handler.command = /^optv?$/i
