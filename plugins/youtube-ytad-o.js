cb46392d42568975a3c865c119e4670bbc44b005
dinuboza <121083548+dinuboza@users.noreply.github.com>
2023-03-26 09:33:39 +0530

    Update youtube-ytad-o.js

--- plugins/youtube-ytad-o.js
+++ plugins/youtube-ytad-o.js
@@ -5,7 +5,7 @@ let handler = async (m, { conn, groupMetadata, usedPrefix, text, args, command }
  `
 let buttons = [{ buttonText: { displayText: 'Audio' }, buttonId: `${usedPrefix}yta ${text} ` }, { buttonText: { displayText: 'Document' }, buttonId: `${usedPrefix}adoc ${text} ` }]
 							 
-let msg = await conn.sendMessage(m.chat, { image: { url: 'https://i.ibb.co/cc8jfkX/01.jpg' }, caption: capt, footer: '_*Whatsapp bot by AHMAD ALI*_', buttons }, { quoted: m })
+let msg = await conn.sendMessage(m.chat, { image: { url: 'https://i.ibb.co/T1DqdYz/thumbnail.jpg' }, caption: capt, footer: '_*Whatsapp bot by Dinuwa Official²⁰²³*_', buttons }, { quoted: m })
 }
 
 handler.command = /^opta?$/i
